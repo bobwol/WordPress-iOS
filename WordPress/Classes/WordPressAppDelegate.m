e7aac8a9c1f671a43f13d80b9f3f1c7e08ad458f
Brad Angelcyk <brad@angelcyk.com>
2014-04-22 13:54:44 -0400

    adds taplytics API key to app

--- WordPress/Classes/WordPressAppDelegate.m
+++ WordPress/Classes/WordPressAppDelegate.m
@@ -7,6 +7,7 @@
 #import <HockeySDK/HockeySDK.h>
 #import <UIDeviceIdentifier/UIDeviceHardware.h>
 #import <Helpshift/Helpshift.h>
+#import <Taplytics/Taplytics.h>
 
 #import "WordPressAppDelegate.h"
 #import "ContextManager.h"
@@ -127,6 +128,8 @@ static NSInteger const IndexForMeTab = 2;
     
     [self showWelcomeScreenIfNeededAnimated:NO];
 
+    [Taplytics startTaplyticsAPIKey:@"9f7ac3d13b5b05eb2a1764535b1fe09eeaa7f705"];
+    
     [Helpshift installForApiKey:[WordPressComApiCredentials helpshiftAPIKey] domainName:[WordPressComApiCredentials helpshiftDomainName] appID:[WordPressComApiCredentials helpshiftAppId]];
     
     return YES;
